dd58c3d85a441ea2abb1972797660aea6703d199
Lavish <lalitchoudhary112000@gmail.com>
2025-09-04 20:08:06 +0530

    feat: configure Supabase client and server setup

--- src/utils/supabase/middleware.ts
+++ src/utils/supabase/middleware.ts
@@ -3,7 +3,7 @@ import { type NextRequest, NextResponse } from "next/server";
 import type { SupabaseClient } from '@supabase/supabase-js';
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_DEFAULT_KEY;
 
 export const createClient = (request: NextRequest): { supabase: SupabaseClient; response: NextResponse } => {
   // Create an unmodified response
--- src/utils/supabase/server.ts
+++ src/utils/supabase/server.ts
@@ -1,9 +1,10 @@
 import { createClient as createSupabaseClient } from '@supabase/supabase-js'
 
 import type { SupabaseClient } from '@supabase/supabase-js';
+import { cookies } from 'next/dist/server/request/cookies';
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_DEFAULT_KEY;
 
 export const createClient = async (): Promise<SupabaseClient> => {
  const cookieStore = await cookies();
